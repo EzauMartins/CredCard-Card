403f394148d0e764bbf53bbe9625153cb13d2c4d
= <=>
2023-01-25 22:07:03 -0400

    buscar cartoes por cliente

--- src/main/java/io/github/credcardtraing/mscartoes/application/CartoesResource.java
+++ src/main/java/io/github/credcardtraing/mscartoes/application/CartoesResource.java
@@ -1,13 +1,16 @@
 package io.github.credcardtraing.mscartoes.application;
 
 import io.github.credcardtraing.mscartoes.domain.Cartao;
+import io.github.credcardtraing.mscartoes.domain.ClienteCartao;
 import io.github.credcardtraing.mscartoes.represetation.CartaoSaveRequest;
+import io.github.credcardtraing.mscartoes.represetation.CartoesPorClienteResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("cartoes")
@@ -15,6 +18,7 @@ import java.util.List;
 public class CartoesResource {
 
     private final CartaoService service;
+    private final ClienteCartaoService clienteCartaoService;
 
     @GetMapping
     public String status(){
@@ -30,5 +34,14 @@ public class CartoesResource {
     public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda){
         List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
         return ResponseEntity.ok(list);
+    }
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesByCliente(
+            @RequestParam("cpf") String cpf){
+        List<ClienteCartao> lista = clienteCartaoService.listCartoesByCpf(cpf);
+        List<CartoesPorClienteResponse> resultList =
+                lista.stream().map(CartoesPorClienteResponse::fromModel).collect(Collectors.toList());
+
+        return ResponseEntity.ok(resultList);
     }
 }
---
+++ src/main/java/io/github/credcardtraing/mscartoes/application/ClienteCartaoService.java
@@ -0,0 +1,20 @@
+package io.github.credcardtraing.mscartoes.application;
+
+
+import io.github.credcardtraing.mscartoes.domain.ClienteCartao;
+import io.github.credcardtraing.mscartoes.infra.repository.ClienteCartaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteCartaoService {
+
+    private final ClienteCartaoRepository repository;
+
+    public List<ClienteCartao> listCartoesByCpf(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
