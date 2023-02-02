5cb5e0965d5f473d631bcbbf82791f223ce1bff1
= <=>
2023-02-01 22:04:11 -0400

    config AMQP rabbit Subscribe

--- src/main/java/io/github/credcardtraing/mscartoes/infra/repository/mqueue/EmissaoCartaoSubscribe.java
+++ src/main/java/io/github/credcardtraing/mscartoes/infra/repository/mqueue/EmissaoCartaoSubscribe.java
@@ -1,15 +1,39 @@
 package io.github.credcardtraing.mscartoes.infra.repository.mqueue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.credcardtraing.mscartoes.domain.Cartao;
+import io.github.credcardtraing.mscartoes.domain.ClienteCartao;
+import io.github.credcardtraing.mscartoes.domain.DadosSolicitacaoCartao;
+import io.github.credcardtraing.mscartoes.infra.repository.CartaoRepository;
+import io.github.credcardtraing.mscartoes.infra.repository.ClienteCartaoRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class EmissaoCartaoSubscribe {
 
+    private final CartaoRepository cartaoRepository;
+    private final ClienteCartaoRepository clienteCartaoRepository;
     @RabbitListener(queues = "${mq.queue.emissao-cartoes}")
-    public void receberSolicitacaoEmissao(@Payload String payload){
-        System.out.println(payload);
+    public void receberSolicitacaoEmissao(@Payload String payload) {
+        try {
+            var mapper = new ObjectMapper();
+            DadosSolicitacaoCartao dados = mapper.readValue(payload, DadosSolicitacaoCartao.class);
+            Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+            ClienteCartao clienteCartao = new ClienteCartao();
+            clienteCartao.setCartao(cartao);
+            clienteCartao.setCpf(dados.getCpf());
+            clienteCartao.setLimite(dados.getLimiteLiberado());
+
+            clienteCartaoRepository.save(clienteCartao);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
